67035f90779ffe11263de2fb2b267db12ce04a28
Sebas Túnchez <tun231359@uvg.edu.gt>
2026-02-27 02:43:23 -0600

    refactor: centralized configurations

---
+++ config.py
@@ -0,0 +1,15 @@
+# Archivo de centralización de configuraciones y constantes globales
+
+# Ruta del archivo de configuración de la Máquina de Turing
+CONFIGURACION = "machines/fibonacci_config.yaml"
+
+# Ruta del directorio de salida para los resultados de las simulaciones
+OUTPUT_DIR = "outputs"
+
+# --- Configuraciones para para máquinas que generan cadenas
+
+# Imprimir cadena resultante
+PRINT_RESULT = True
+
+# Imprimir longitud de la cadena resultante (requiere PRINT_RESULT = True)
+PRINT_LENGTH = True
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from parser.loader import MTConfigLoader
 from core.turing_machine import TuringMachine
 from core.simulation import Simulator
+from config import CONFIGURACION, OUTPUT_DIR, PRINT_RESULT, PRINT_LENGTH
 
 def main():
     """
@@ -18,21 +19,32 @@ def main():
         ValueError: Si la configuración es inválida o incompleta.
     """
     # Cambiar el nombre del archivo para usar una configuración diferente
-    loader = MTConfigLoader("machines/config2.yaml")
+    loader = MTConfigLoader(CONFIGURACION)
     config = loader.load()
 
     machine = TuringMachine(config)
     simulator = Simulator(machine)
 
-    os.makedirs("outputs", exist_ok=True)
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
 
     simulation_counter = 1
 
+    def clean_tape_content(tape):
+        """Extrae el contenido de la cinta sin blanks y sin símbolos de control."""
+        # Eliminar B's que representan blanks
+        content = ''.join([str(sym) for sym in tape.tape if sym is not None])
+        # Calcular la longitud del string
+        length = len(content)
+        if PRINT_LENGTH:
+            return content.strip() + f" = {length}"
+        
+        return content.strip()
+
     for s in config["simulation_strings"]:
-        accepted, log_lines = simulator.run_string(s)
+        accepted, log_lines, final_tape = simulator.run_string(str(s))
 
         output_path = os.path.join(
-            "outputs",
+            OUTPUT_DIR,
             f"simulation_{simulation_counter}.txt"
         )
 
@@ -40,13 +52,18 @@ def main():
             for line in log_lines:
                 f.write(line + "\n")
 
-            f.write("\n" + "-"*30 + "\n")
-            f.write("RESULTADO FINAL:\n")
+            f.write("\n" + "-"*40 + "\n")
+            if PRINT_RESULT:
+                final_content = clean_tape_content(final_tape)
+                f.write(f"RESULTADO FINAL: {final_content}\n")
+            else:
+                f.write("RESULTADO FINAL:\n")
+            
             if accepted:
                 f.write("Cadena ACEPTADA ✔\n")
             else:
                 f.write("Cadena RECHAZADA ✘\n")
-            f.write("-"*30 + "\n")
+            f.write("-"*40 + "\n")
 
         print(f"Archivo generado: {output_path}")
         simulation_counter += 1
